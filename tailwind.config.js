c948db03bd420b764023f382ac598d4e2d48c48c
shamim888 <shamimnw99@gmail.com>
2022-02-14 11:00:45 +0600

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        
+        ikoins: {
+          yellow: '#ffc301',
+          yellowBdr: '#dfa300',
+          card: '#2b2b2b',
+          bgColor: '#202020'
+
+        },
+        
+        orange: {
+          50: '#FFF7ED',
+          100: '#FFEDD5',
+          200: '#FED7AA',
+          300: '#FDBA74',
+          400: '#FB923C',
+          500: '#F97316',
+          600: '#EA580C',
+          700: '#C2410C',
+          800: '#9A3412',
+          900: '#7C2D12',
+        },
+        
+        Lime: {
+          50: '#f7fee7',
+          100: '#ecfccb',
+          200: '#d9f99d',
+          300: '#bef264',
+          400: '#a3e635',
+          500: '#84cc16',
+          600: '#65a30d',
+          700: '#4d7c0f',
+          800: '#3f6212',
+          900: '#365314',
+        }
+        },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
